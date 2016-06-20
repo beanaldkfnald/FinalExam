8baf4bbe593fa45a0a221f602085f49214000093
beanaldkfnald <ranswkgkwlak@daum.net>
2016-06-20 22:33:41 +0900

    Add files via upload

---
+++ Temple.java
@@ -0,0 +1,28 @@
+public class temple{
+  
+  public static void main(String[] args){
+    //Don't change this line
+    Scanner in = new Scanner(System.in);
+    
+    int monk = 1;
+    int Money = 10;
+    int Zen point = 10;
+    System.out.println("This is Monk raise Game");
+    System.out.println("When Monk Zen point is to fill 100 is the end of the game");
+    System.out.println("There are quests to fill the Zen poimt 100");
+    System.out.println("Start Game");
+  }
+}
+  
+    System.out.println("Hey monk do you want qusets (Y/N)");
+    String monk = in.nextLine();
+    if(monk.equals("N")){
+      break;
+    else(monk.equals("Y")){
+      System.out.println("Meditatin is 1 hours + mana 90");
+      System.out.println("monk total Zen point 100!");
+      
+      int monk total Zen point = 100;
+      break;
+    }
+    }
